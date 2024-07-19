037b202a984c2c77779b7c538d3c5a8bb8709976
eggnocent <egiwiratamaaa@gmail.com>
2024-07-19 11:01:27 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Egi Wiratama"
+	fmt.Println(name)
+
+	name = "Egi Wira"
+	fmt.Println(name)
+}
+
+// menggunakan kata kunci :=
+// func main() {
+
+// 	name := "Egi Wiratama"
+// 	fmt.Println(name)
+
+// 	name = "Egi Wira"
+// 	fmt.Println(name)
+// }
+
+// tanpa disebut tipe datanya
+// func main() {
+// 	var name = "Egi Wiratama"
+// 	fmt.Println(name)
+
+// 	name = "Egi Wira"
+// 	fmt.Println(name)
+// }
