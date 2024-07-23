3fc9c0ea066c0a51fadec2401d6127886b80f3b3
eggnocent <egiwiratamaaa@gmail.com>
2024-07-23 13:13:27 +0700

    23/07/2024

---
+++ return_multiple_values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func getFullName() (string, string) {
+	return "Egi", "Wiratama"
+}
+func main() {
+	firstName, lastName := getFullName()
+	fmt.Println(firstName, lastName)
+}
